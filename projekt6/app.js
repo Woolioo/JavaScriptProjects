00360fb078c6140bc3a2ac2eddf4d8eebf18be28
Woolioo <michucity17@gmail.com>
2023-09-22 20:47:17 +0200

    Todays day and random fact

---
+++ projekt6/app.js
@@ -0,0 +1,19 @@
+const currentDay = document.querySelector('.current-day');
+const funFact = document.querySelector('.fun-fact');
+const facts = [
+    `Crocodile can't stick out its tongue.`,
+    'Each person spent about half an hour as a single cell.',
+    'Sound travels 15 times faster through steel than through air.',
+    'Sloths take two weeks to digest food.',
+    'Gorillas sleep up to fourteen hours a day.',
+    `Chameleon's tongue is twice as long as its body.`,
+    'Chinese consume about 80 billion chopsticks in a year.',
+    'To climb the Eiffel Tower you have to climb as many as 1,710 steps.'
+];
+const day = new Date();
+currentDay.textContent = day.toLocaleString("eng", { weekday: "long" });
+const showRand = () => {
+    const number = Math.floor(Math.random() * facts.length);
+    funFact.textContent = facts[number];
+};
+showRand();
