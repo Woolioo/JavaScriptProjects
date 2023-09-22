80cb49142746e69c328922023a6375bcfdebc24e
Woolioo <michucity17@gmail.com>
2023-09-22 20:22:10 +0200

    simple burger menu

---
+++ projekt5/app.js
@@ -0,0 +1,11 @@
+const burgerBtn = document.querySelector('.burger');
+const barsIco = document.querySelector('.fa-bars');
+const xIco = document.querySelector('.fa-times');
+const nav = document.querySelector('nav ul');
+const burgerMenu = () => {
+    nav.classList.toggle('active');
+    burgerBtn.classList.toggle('active');
+    barsIco.classList.toggle('hide');
+    xIco.classList.toggle('hide');
+};
+burgerBtn.addEventListener('click', burgerMenu);
