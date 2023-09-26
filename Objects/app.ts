ff6a2c300de1298fe7f0309679a7bb63934fca95
Woolioo <michucity17@gmail.com>
2023-09-26 11:34:59 +0200

    learning objects

---
+++ Objects/app.ts
@@ -0,0 +1,31 @@
+const newUser = {
+	name: 'Micky',
+	age: 20,
+	car: {
+		brand: 'BMW',
+		model: 'M3',
+		color: 'black',
+		sound() {
+			console.log(`wrrrrum`)
+		},
+	},
+}
+
+newUser['country'] = 'Poland'
+newUser['fav-food'] = 'kebab'
+
+const catName = 'Luna'
+const catAge = '5'
+
+const cat = {
+	catName,
+	catAge,
+}
+
+console.log(newUser)
+newUser.car.sound()
+console.log(cat)
+
+for (const data in newUser) {
+	console.log(data + ': ' + newUser[data])
+}
