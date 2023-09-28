8b1e39ea528846ef5672f69833d75c7bd6a7f1ab
Michał Sobolewski <michucity17@gmail.com>
2023-09-28 10:28:04 +0200

    basics

--- Objects/learning/app.js
+++ Objects/learning/app.js
@@ -1,55 +1,56 @@
 const newUser = {
-    name: 'Micky',
-    age: 20,
-    car: {
-        brand: 'BMW',
-        model: 'M3',
-        color: 'black',
-        sound() {
-            console.log(`wrrrrum`);
-        },
-    },
-};
-newUser['country'] = 'Poland';
-newUser['fav-food'] = 'kebab';
-const catName = 'Luna';
-const catAge = '5';
+	name: 'Micky',
+	age: 20,
+	car: {
+		brand: 'BMW',
+		model: 'M3',
+		color: 'black',
+		sound() {
+			console.log(`wrrrrum`)
+		},
+	},
+}
+newUser['country'] = 'Poland'
+newUser['fav-food'] = 'kebab'
+const catName = 'Luna'
+const catAge = '5'
 const cat = {
-    catName,
-    catAge,
-};
-console.log(newUser);
-newUser.car.sound();
-console.log(cat);
-for (const data in newUser) { //for in 
-    console.log(data + ': ' + newUser[data]);
+	catName,
+	catAge,
+}
+console.log(newUser)
+newUser.car.sound()
+console.log(cat)
+for (const data in newUser) {
+	//for in
+	console.log(data + ': ' + newUser[data])
 }
 function User(name, age) {
-    this.name = name;
-    this.age = age;
+	this.name = name
+	this.age = age
 }
-const nextUser = new User('Klaudia', 23); //used of 'new'
-const nextUser2 = new User('Kasia', 24);
-console.log(nextUser);
-console.log(nextUser2);
+const nextUser = new User('Klaudia', 23) //used of 'new'
+const nextUser2 = new User('Kasia', 24)
+console.log(nextUser)
+console.log(nextUser2)
 User.prototype.hello = function () {
-    console.log(`Cześć ${this.name}`);
-};
-nextUser.hello();
-nextUser2.hello();
+	console.log(`Cześć ${this.name}`)
+}
+nextUser.hello()
+nextUser2.hello()
 
-const ob = { 
+const ob = {
 	number: 123,
-	showNumber () {
-		console.log(this.number);
+	showNumber() {
+		console.log(this.number)
 	},
-	showNmber2 () { 
-		const test = () => { 
-			console.log(this.number);
-			console.log(this);
+	showNmber2() {
+		const test = () => {
+			console.log(this.number)
+			console.log(this)
 		}
 		test()
-	}
+	},
 }
 
 ob.showNumber()
--- Objects/learning/app.ts
+++ Objects/learning/app.ts
@@ -1,4 +1,5 @@
-const newUser = {       //basic object
+const newUser = {
+	//basic object
 	name: 'Micky',
 	age: 20,
 	car: {
@@ -17,7 +18,8 @@ newUser['fav-food'] = 'kebab'
 const catName = 'Luna'
 const catAge = '5'
 
-const cat = {       // using names of variables istead of key and value
+const cat = {
+	// using names of variables istead of key and value
 	catName,
 	catAge,
 }
@@ -26,39 +28,42 @@ console.log(newUser)
 newUser.car.sound()
 console.log(cat)
 
-for (const data in newUser) {       //for in 
+for (const data in newUser) {
+	//for in
 	console.log(data + ': ' + newUser[data])
 }
 
-function User(name, age) {      //consturctor
+function User(name, age) {
+	//consturctor
 	this.name = name
 	this.age = age
 }
 
-const nextUser = new User('Klaudia',23)     //used of 'new'
+const nextUser = new User('Klaudia', 23) //used of 'new'
 const nextUser2 = new User('Kasia', 24)
-console.log(nextUser);
-console.log(nextUser2);
+console.log(nextUser)
+console.log(nextUser2)
 
-User.prototype.hello = function() {       //prototype
-    console.log(`Cześć ${this.name}`);
+User.prototype.hello = function () {
+	//prototype
+	console.log(`Cześć ${this.name}`)
 }
 
 nextUser.hello()
 nextUser2.hello()
 
-const ob = { 
+const ob = {
 	number: 123,
-	showNumber () {
-		console.log(this.number);
+	showNumber() {
+		console.log(this.number)
 	},
-	showNmber2 () { 
-		const test = () => { 
-			console.log(this.number);
-			console.log(this);
+	showNmber2() {
+		const test = () => {
+			console.log(this.number)
+			console.log(this)
 		}
 		test()
-	}
+	},
 }
 
 ob.showNumber()
