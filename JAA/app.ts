fc82b52f35f0ce5da8c134dee0c6084b50537c6e
Woolioo <michucity17@gmail.com>
2023-10-02 15:09:29 +0200

    Async function

--- JAA/app.ts
+++ JAA/app.ts
@@ -8,3 +8,49 @@ btn.addEventListener('click', () => {
 		.then(data => img.setAttribute('src', data.message))
 		.catch(err => console.error(err))
 })
+
+//  	PROMISES
+
+const checkAge = age => {
+	return new Promise<void>((resolve, reject) => {
+		if (age >= 18) {
+			resolve()
+		} else {
+			reject(`You're not old enough`)
+		}
+	})
+}
+
+const doubleCheck = () => {
+	return new Promise(resolve => {
+		console.log(`Checking one more time...`)
+		setTimeout(() => {
+			resolve('Okay, everything is fine')
+		}, 1000)
+	})
+}
+// checkAge(52)
+// 	.then(() => {
+// 		console.log('You can come in')
+// 		return doubleCheck()
+// 	})
+// 	.then(res => console.log(res))
+// 	.then(() => {
+// 		console.log(`You've passed the test`)
+// 	})
+// 	.catch(error => console.error(error))
+
+//  ASYNC & AWAIT
+
+async function newTest() {
+	try {
+		await checkAge(22)
+		console.log('You can come in')
+		await doubleCheck()
+		console.log('Okay, everything is fine')
+		console.log("You've passed the test")
+	} catch {
+		console.error(`You're not old enoug`)
+	}
+}
+newTest()
