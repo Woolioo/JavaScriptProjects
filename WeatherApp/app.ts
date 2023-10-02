26e821fa6536d5cd02c0a9a852c433bde737976c
Woolioo <michucity17@gmail.com>
2023-10-02 22:47:49 +0200

    WeatherAPP 1.0

---
+++ WeatherApp/app.ts
@@ -0,0 +1,12 @@
+const input = document.querySelector('input')
+const button = document.querySelector('button')
+const cityName = document.querySelector('.city-name')
+const warning = document.querySelector('.warning')
+const photo = document.querySelector('.photo')
+const weather = document.querySelector('.weather')
+const temperature = document.querySelector('.temperature')
+const humidity = document.querySelector('.humidity')
+
+const API_LINK = 'https://api.openweathermap.org/data/2.5/weather?q='
+const API_KEY = 'e2dedab8e36299786b9c4e04088115f8'
+const API_UNITS = '&units=metric'
